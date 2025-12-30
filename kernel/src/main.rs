d346e57443b757ea1fc5cddf89d85b53afcddf44
Antorof1 <antorof@proton.me>
2025-12-30 20:00:12 +0100

    Add bootloader crate support

---
+++ kernel/src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+use bootloader_api::{BootInfo, entry_point};
+use core::panic::PanicInfo;
+
+entry_point!(kernel_main);
+
+fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
