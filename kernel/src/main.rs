9821d0aca9eda6ccfc76dc0f4a4dd185d93cad1d
Antorof1 <antorof@proton.me>
2025-12-31 14:30:52 +0100

    Add serial output

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,11 +2,26 @@
 #![no_main]
 
 use bootloader_api::{BootInfo, entry_point};
+use core::fmt::Write;
 use core::panic::PanicInfo;
+use uart_16550::SerialPort;
+
+fn serial() -> SerialPort {
+    const SERIAL_IO_PORT: u16 = 0x3F8;
+
+    let mut port = unsafe { SerialPort::new(SERIAL_IO_PORT) };
+    port.init();
+
+    port
+}
 
 entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    let mut serial_port = serial();
+
+    writeln!(serial_port, "{boot_info:?}").unwrap();
+
     loop {}
 }
 
